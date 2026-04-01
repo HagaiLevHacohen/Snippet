e1a8e48ae9571a2d9da91c13680b95a710bb1893
Hagai Lev Hacohen <hagai4000@gmail.com>
2026-04-01 15:43:56 +0300

    fixed the profile header for different device sizes

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -66,7 +66,7 @@ const createConversationMutation = useMutation({
   return (
     <div className='flex flex-col w-full xl:w-7/10 xl:min-w-75 bg-gray-800 rounded-lg p-4 gap-8'>
 
-      <div className='flex w-full items-center justify-between gap-10'>
+      <div className='flex w-full items-center justify-between gap-5'>
         <div className="flex items-center gap-4">
             {/* Avatar */}
             <img
@@ -82,21 +82,21 @@ const createConversationMutation = useMutation({
             </div>
         </div>
         {isOwnProfile ? 
-        <Link to="/settings" className='h-10 sm:w-30 bg-violet-500 hover:bg-violet-600 text-white p-2 text-center rounded-lg'>Edit Profile</Link>
+        <Link to="/settings" className='h-10 text-sm sm:text-lg sm:w-30 bg-violet-500 hover:bg-violet-600 text-white sm:p-2 text-center rounded-lg'>Edit Profile</Link>
         :
         <div className='flex flex-col sm:flex-row gap-4'>
-          <button onClick={() => createConversationMutation.mutate({ recipientId: user.id })} className='h-10 sm:w-30 bg-violet-500 hover:bg-violet-600 text-white px-3 py-1 rounded-lg'>Message</button>
-          <button onClick={user.followStatus === "FOLLOWING"? () => handleFollowClick("unfollow") : () => handleFollowClick("follow")} className={`h-10 sm:w-30 ${user.followStatus === "FOLLOWING" ? 'bg-gray-500 hover:bg-red-600' : 'bg-violet-500 hover:bg-violet-600'} text-white px-3 py-1 rounded-lg`}>   {user.followStatus === "FOLLOWING" ? "Following" : user.followStatus === "REQUESTED" ? "Requested" : "Follow"} </button>
+          <button onClick={() => createConversationMutation.mutate({ recipientId: user.id })} className='h-10 text-sm sm:text-lg sm:w-30 bg-violet-500 hover:bg-violet-600 text-white sm:px-3 sm:py-1 rounded-lg'>Message</button>
+          <button onClick={user.followStatus === "FOLLOWING"? () => handleFollowClick("unfollow") : () => handleFollowClick("follow")} className={`h-10 text-sm sm:text-lg sm:w-30 ${user.followStatus === "FOLLOWING" ? 'bg-gray-500 hover:bg-red-600' : 'bg-violet-500 hover:bg-violet-600'} text-white sm:px-3 sm:py-1 rounded-lg`}>   {user.followStatus === "FOLLOWING" ? "Following" : user.followStatus === "REQUESTED" ? "Requested" : "Follow"} </button>
         </div>
         }
       </div>
 
       <p className="text-gray-400 px-8">{user.status ?? "No status available"}</p>
 
-      <div className="flex justify-between text-sm">
-        <div>
-            <span className='font-bold'>{user._count?.followers}</span> Followers
-            <span className='font-bold ml-4'>{user._count?.following}</span> Following
+      <div className="flex justify-between text-sm sm:text-lg">
+        <div className='flex flex-col sm:flex-row gap-2'>
+            <span>{user._count?.followers} Followers</span>
+            <span>{user._count?.following} Following</span>
         </div>
         <span className='text-gray-400'>Joined: {new Date(user.createdAt).toLocaleDateString()}</span>
       </div>
